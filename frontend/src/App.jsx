31000570210d79810c636996faad0ef337efac73
Abhin krishna M P <abhinkrishnamp0@gmail.com>
2024-08-02 19:02:14 +0530

    project initiallized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+
+    <div className='app'>
+
+      <Routes>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
